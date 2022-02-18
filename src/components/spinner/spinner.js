241eec594d00e6433723f054a0cd58d390df8e14
Vladimir <slight1989@gmail.com>
2022-02-18 19:54:29 +0400

    init: Init project Contains main file structure Completed API services done: RandomPlanet Component completely done

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-double-ring-k192yzt03r">
+      <div className="ldio-h9bxm3o90aj">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
